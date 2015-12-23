611e9fd246de5e6c76e0a5aac702f951f2d7f9cf
kannanshan <kannanscse@gmail.com>
2015-12-23 14:06:48 +0530

    Model changes

--- SAS_POC/POC_V1/models.py
+++ SAS_POC/POC_V1/models.py
@@ -2,4 +2,17 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Ticket_details(models.Model):
+    ticket_id = models.IntegerField()
+    subject = models.TextField()
+    description = models.TextField()
+    status = models.CharField(max_length=50)
+    source = models.CharField(max_length=50)
+    priority = models.IntegerField()
+    type = models.CharField(max_length=50)
+    agent = models.CharField(max_length=50)
+    group_name = models.CharField(max_length=50)
+    created_at = models.DateField()
+
+    def __str__(self):              # __unicode__ on Python 2
+        return self.ticket_id
